36146ad79c2f402f533b4ad788ef238521c006ec
EDICON <hslee.edicon@gmail.com>
2020-07-03 12:18:14 +0900

    -my version build

--- lib/chart.dart
+++ lib/chart.dart
@@ -6,7 +6,6 @@ import 'package:provider/provider.dart';
 class MyChart extends StatefulWidget {
   @override
   _MyChartState createState() => _MyChartState();
-
 }
 
 class _MyChartState extends State<MyChart> {
@@ -19,19 +18,30 @@ class _MyChartState extends State<MyChart> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<MySchedule>(
-      builder:(context,schedule,_){
-       return PieChart(
-          dataMap:_createData(schedule.stateManagementValue) ,
-          chartRadius: MediaQuery.of(context).size.width / 1.5,
-          showLegends: false,
-          colorList: colorList,
-        );
-      }
-    );
+    final useProfider = true;
+    if ( useProfider) {
+      final schedule = Provider.of<MySchedule>(context);
+      return PieChart(
+        dataMap: _createData(schedule.stateManagementValue),
+        chartRadius: MediaQuery.of(context).size.width / 1.5,
+        showLegends: false,
+        colorList: colorList,
+      );
+    } else {
+      return Consumer<MySchedule>(
+        builder:(context,schedule,_){
+        return PieChart(
+            dataMap:_createData(schedule.stateManagementValue) ,
+            chartRadius: MediaQuery.of(context).size.width / 1.5,
+            showLegends: false,
+            colorList: colorList,
+          );
+        }
+      );
+    }
   }
 
-  static Map<String, double> _createData(double stateManagementValue){
+  static Map<String, double> _createData(double stateManagementValue) {
     Map<String, double> dataMap = Map();
     dataMap.putIfAbsent("State Management", () => stateManagementValue);
     dataMap.putIfAbsent("UI programming", () => 0.3);
